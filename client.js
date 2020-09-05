29d0c34e70ac97d68993ff5b5d61f7c3b2c8e98d
silinh66 <50419718+silinh66@users.noreply.github.com>
2020-09-05 13:18:43 +0700

    Add files via upload

---
+++ client.js
@@ -0,0 +1,15 @@
+var socket = io.connect('http://localhost:8000/');
+socket.on('connect', function(data){
+    socket.emit('join', 'Hello server from client');
+});
+socket.on('thread', function(data){
+    $('#thread').append('<li>'+ data +'</li>');
+});
+
+$('form').submit(function(){
+    var message = $('#message').val();
+    socket.emit('messages',message);
+    this.reset();
+
+    return false;
+});
